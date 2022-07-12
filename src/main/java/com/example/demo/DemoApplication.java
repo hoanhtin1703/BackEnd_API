42addc3f6ff2d9be01a4220379bd70f935678b01
QuanPhan <quanphan2k@gmail.com>
2022-07-12 10:37:07 +0700

    ...

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+import com.example.demo.model.Product_Model;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+
+public class DemoApplication  {
+	List<Product_Model> list;
+//	Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+//	@Autowired
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+}
